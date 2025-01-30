2609fed1c32982f6909365afec965d25bc0e83a4
saveside <savepersonal@proton.me>
2025-01-30 14:04:29 +0300

    Fix formatting in Nix configuration for AGS package management

--- hosts/savew-notebook/ags/default.nix
+++ hosts/savew-notebook/ags/default.nix
@@ -5,12 +5,12 @@
 
 {
   xdg.configFile."ags" = {
-     recursive = true;
-     source = pkgs.fetchFromGitHub {
-       owner = "saveside";
-       repo = "savew-ags";
-       rev = "0df7a2a";
-       sha256 = "sha256-lyZQpM1iW990Lm1Hblct+IN4HZ0O0aflwPOSw2PAzSc=";
-     };
+    recursive = true;
+    source = pkgs.fetchFromGitHub {
+      owner = "saveside";
+      repo = "savew-ags";
+      rev = "0df7a2a";
+      sha256 = "sha256-lyZQpM1iW990Lm1Hblct+IN4HZ0O0aflwPOSw2PAzSc=";
+    };
   };
 }
