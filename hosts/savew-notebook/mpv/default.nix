38956df0b65c1f5fd20decac4713c2c19e3a5c93
saveside <savepersonal@proton.me>
2025-01-30 16:11:53 +0300

    Update mpv configuration with SHA256 hash for repository integrity

--- hosts/savew-notebook/mpv/default.nix
+++ hosts/savew-notebook/mpv/default.nix
@@ -19,8 +19,7 @@
       owner = "Tsubajashi";
       repo = "mpv-settings";
       rev = "f859a9a";
-      sha256 = "";
+      sha256 = "sha256-Wie8FLDvfHCHdF4aa1EW9Bd71kuViFfhUek0MY8kHCw=";
     };
   };
- }
-
+}
