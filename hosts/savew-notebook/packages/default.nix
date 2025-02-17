c10074373dbec7eb7dc711bfb5252f3769477a0e
saveside <savew.sh@proton.me>
2025-02-17 21:30:59 +0300

    feat: replace dolphin with pcmanfm-qt in wrapped packages configuration

--- hosts/savew-notebook/packages/default.nix
+++ hosts/savew-notebook/packages/default.nix
@@ -53,17 +53,17 @@
   };
 
   #~ other wrapped packages ~#
-  options.wrappedPkgs.dolphin = lib.mkOption {
+  options.wrappedPkgs.pcmanfm-qt = lib.mkOption {
     default = (
       config.lib.nixGL.wrap (
         pkgs.symlinkJoin {
-          name = "dolphin";
-          paths = [ pkgs.kdePackages.dolphin ];
+          name = "pcmanfm-qt";
+          paths = [ pkgs.pcmanfm-qt ];
           buildInputs = [ pkgs.makeWrapper ];
           postBuild = ''
-            wrapProgram $out/bin/dolphin \
-              --set QT_STYLE_OVERRIDE kvantum \
-              --set XDG_CURRENT_DESKTOP KDE
+            wrapProgram $out/bin/pcmanfm-qt \
+              --set XDG_CURRENT_DESKTOP KDE \
+              --set QT_QPA_PLATFORMTHEME qt6ct 
           '';
         }
       )
